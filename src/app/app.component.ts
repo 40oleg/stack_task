3c3b38981f06f6f100ee108b1ca25a94df54f026
40oleg <oleg.merkulov701@gmail.com>
2024-06-18 22:26:21 +0300

    feat: added native angular service worker integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, isDevMode } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainGroupComponent } from './main-group/main-group.component';
+import { SwUpdate } from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
@@ -15,4 +16,7 @@ import { MainGroupComponent } from './main-group/main-group.component';
 })
 export class AppComponent {
   title = 'stack_task';
+  constructor(public swUpdate: SwUpdate) {
+    alert(this.swUpdate.isEnabled)
+  }
 }
